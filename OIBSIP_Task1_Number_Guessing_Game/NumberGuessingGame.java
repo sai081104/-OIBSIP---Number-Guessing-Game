d1ad0be58f8ef8602be486293b8f4d66f9e68677
sai081104 <saimunde0804@gmail.com>
2025-04-16 15:03:12 +0530

    Add files via upload

---
+++ OIBSIP_Task1_Number_Guessing_Game/NumberGuessingGame.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        int numberToGuess = rand.nextInt(100) + 1;
+        int guess;
+        int attempts = 0;
+
+        System.out.println("🎯 Welcome to Number Guessing Game!");
+        System.out.println("Guess a number between 1 and 100");
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+            attempts++;
+
+            if (guess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("🎉 Congratulations! You guessed it in " + attempts + " attempts.");
+            }
+
+        } while (guess != numberToGuess);
+
+        sc.close();
+    }
+}
